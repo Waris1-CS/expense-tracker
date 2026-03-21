c1117b1e6c78764538cf241a2f0a3a8a573fb2a7
Waris <warisleckon@gmail.com>
2026-03-21 17:16:01 +0100

    feat: remove getTotal function

--- expense-tracker.js
+++ expense-tracker.js
@@ -5,14 +5,7 @@ function addExpense(name, amount) {
     console.log(`Added expense: ${name} - $${amount}`)
 }
 
-function getTotalExpense() {
-    const total = expense.reduce((sum, item) => sum + item.amount, 0);
-    return `Total Expense: $${total}`
-}
-
 addExpense("Airtime", 100);
 addExpense("Food", 210);
 addExpense("Groceries", 25);
 addExpense("Transport", 50);
-
-console.log(getTotalExpense())
