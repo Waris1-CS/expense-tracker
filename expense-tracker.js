e70955833111b0e91b4c3570dc1e1d6726beb38b
Waris <warisleckon@gmail.com>
2026-03-22 07:13:19 +0100

    .\expense-tracker.js

--- expense-tracker.js
+++ expense-tracker.js
@@ -9,3 +9,9 @@ addExpense("Airtime", 100);
 addExpense("Food", 210);
 addExpense("Groceries", 25);
 addExpense("Transport", 50);
+
+function getTotalExpense() {
+    return expense.reduce((sum , item) => sum + item.amount, 0)
+}
+
+console.log(`The total expense is: $${getTotalExpense()}`)
